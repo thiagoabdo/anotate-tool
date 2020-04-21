cb662150061a3ff422773ec2d936cf53dcd778a2
Thiago Abdo <tjabdo@inf.ufpr.br>
2020-04-21 11:42:07 -0300

    change double quotes to single
    Signed-off-by: Thiago Abdo <tjabdo@inf.ufpr.br>

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :datasets
   get 'pages/home'
   devise_for :users
-  root to: "pages#home"
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
