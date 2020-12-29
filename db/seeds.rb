d0f4e06c01940089039361dc7b48c0b3f642dd2b
Thiago Abdo <tj.xd@hotmail.com>
2020-12-29 13:55:20 -0300

    trying to create a database seed
    Signed-off-by: Thiago Abdo <tj.xd@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+bob = User.create(:email => 'test@c3sl.ufpr.br', :password => '123mudar')
+
+ds = Dataset.create(:name => 'MeuProjeto', :description => 'Esse é o projeto do Bob')
+rs = Role.create([{ :user_id => bob.id, :dataset_id => ds.id, :role => '0' }])
